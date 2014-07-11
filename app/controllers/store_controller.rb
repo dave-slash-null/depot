32c0d6987be033e8b8536ba569dd599fa199be5e
Dave Rowan <drowan99@gmail.com>
2014-07-11 01:16:33 -0400

    Iteration D (chapter 9) completed, including RailsPlayTime. Still need to go back and complete chapter 8 RailsPlayTime.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	if session[:counter].nil?
+  		session[:counter] = 0
+  	end
+  	session[:counter] += 1
+   	@looks = session[:counter]
   end
 end
